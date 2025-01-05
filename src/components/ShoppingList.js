c69d7de49c2029bcdfcdb04330a2433e6c52a646
Arthur Ish <arthurishdev@gmail.com>
2025-01-04 20:21:56 -0800

    filtered with conditional rendering

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedcategory] = useState("All");
+
+  function handleCategorySelect(event){
+    setSelectedcategory(event.target.value);
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleCategorySelect} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -14,7 +21,8 @@ function ShoppingList({ items }) {
       </div>
       <ul className="Items">
         {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+          selectedCategory === item.category || selectedCategory === "All" ? <Item key={item.id} name={item.name} category={item.category} /> : null
+          
         ))}
       </ul>
     </div>
